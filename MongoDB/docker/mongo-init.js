a92ad43d21c3a169e543f0e50f405bab02e69ce9
Viktor Chupak <50545334+vikchupak@users.noreply.github.com>
2026-03-08 21:21:49 +0200

    Update mongo-init.js

--- MongoDB/docker/mongo-init.js
+++ MongoDB/docker/mongo-init.js
@@ -1,3 +1,6 @@
+// rs.initiate() is run only once when you first create the replica set
+// The rs config is preserved in /data/db volume, so re-starting/re-creating container would not run this again
+
 rs.initiate({
   _id: 'rs0',
   members: [{ _id: 0, host: '127.0.0.1:27017' }],
